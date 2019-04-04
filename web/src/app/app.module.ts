afe75481a7bff1ea9954f7971c62790d6a21778d
alexandria.graham15@gmail.com <graham.1012@osu.edu>
2019-04-04 12:03:23 -0400

    created nav bar and routing

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PatientsComponent } from './patients/patients.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { AppointmentsComponent } from './appointments/appointments.component';
+import { IllnessComponent } from './illness/illness.component';
+import { MedicationsComponent } from './medications/medications.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    PatientsComponent,
+    DoctorsComponent,
+    AppointmentsComponent,
+    IllnessComponent,
+    MedicationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
